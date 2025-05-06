2e649ddb35c37e20f943330a163c71d9a351057f
Jason Moore <jmoore@austwa.com>
2025-05-06 20:09:01 +0800

    Create set_hba.sh

---
+++ postgres16/set_hba.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Check if postgres rules exist
+if grep -q "127.0.0.1/32" /var/lib/postgresql/data/pgdata/pg_hba.conf; then
+  echo "172.17.0.1/8 exists";
+else
+  echo "host    all             all             127.0.0.1/32            scram-sha-256" >> /var/lib/postgresql/data/pgdata/pg_hba.conf
+fi
+
+if grep -q "100.112.0.0/16" /var/lib/postgresql/data/pgdata/pg_hba.conf; then
+  echo "100.112.0.0/16 exists";
+else
+  echo "host    all             all             100.112.0.1/16            scram-sha-256" >> /var/lib/postgresql/data/pgdata/pg_hba.conf
+fi
