6e0919e369700448307611e46a2acff2cb6feef9
Jason Moore <jmoore@austwa.com>
2025-11-05 16:37:38 +0800

    Update startup.sh

--- geoserver_2_25/startup.sh
+++ geoserver_2_25/startup.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 echo "Welcome to GeoServer $GEOSERVER_VERSION"
 
+if [ -z "${ADMIN_GEOSERVER_PASSWORD}" ]; then
+  echo "Error: ADMIN_GEOSERVER_PASSWORD is not set or is empty."
+  exit 1
+fi
+sed -i.bak -E "s/(<user[^>]*name=\"admin\"[^>]*password=\")[^\"]*(\"[^>]*>)/\1plain:${ADMIN_GEOSERVER_PASSWORD}\2/" "/opt/geoserver_data/security/usergroup/default/users.xml"
+
 ## Skip demo data
 if [ "${SKIP_DEMO_DATA}" = "true" ]; then
   unset GEOSERVER_REQUIRE_FILE
