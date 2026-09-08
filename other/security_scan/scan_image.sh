541d374d200ef3a11d3c6488fb3cc9e7e8f0d2f1
Jason Moore <jmoore@austwa.com>
2026-09-08 18:00:34 +0800

    Create scan_image.sh

---
+++ other/security_scan/scan_image.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+skopeo copy docker://$DOCKER_IMAGE docker-archive:/tmp/dockerimage-export.tar
+mkdir /tmp/dockerimage/
+
+
+tar -xvof /tmp/dockerimage-export.tar -C /tmp/dockerimage/
+mkdir /tmp/dockerimage/uncompressed
+cd   /tmp/dockerimage/
+
+for f in *.tar; do tar -xvof "$f" -C /tmp/dockerimage/uncompressed/; done
+
+cd /tmp
+
+mkdir /tmp/scans
+syft dir:/tmp/dockerimage/uncompressed/ -o cyclonedx-json="/tmp/scans/sbom.json"
+
+grype /tmp/scans/sbom.json --sort-by=severity --output=template --template=/app/assets/tsv.tmpl --file=/tmp/scans/vulnerabilities.csv
