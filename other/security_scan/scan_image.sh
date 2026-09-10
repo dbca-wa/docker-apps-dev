cf4e3f578fdbc3edb48382133a0442ef6c3c80de
Jason Moore <jmoore@austwa.com>
2026-09-10 11:20:22 +0800

    Update scan_image.sh

--- other/security_scan/scan_image.sh
+++ other/security_scan/scan_image.sh
@@ -1,23 +1,9 @@
 #!/bin/bash
 
-echo "Scanning Image $DOCKER_IMAGE"
-skopeo copy docker://$DOCKER_IMAGE docker-archive:/tmp/dockerimage-export.tar
-mkdir /tmp/dockerimage/
-
-
-tar -xvof /tmp/dockerimage-export.tar -C /tmp/dockerimage/
-mkdir /tmp/dockerimage/uncompressed
-cd   /tmp/dockerimage/
-
-for f in *.tar; do tar -xvof "$f" -C /tmp/dockerimage/uncompressed/; done
-
-cd /tmp
-
+echo "Scanning Image $DOCKER_IMAGE Started"
+skopeo copy docker://$DOCKER_IMAGE oci:/tmp/oci-image:latest
 mkdir /tmp/scans
-# syft dir:/tmp/dockerimage/uncompressed/ -o cyclonedx-json="/tmp/scans/sbom.json"
-# create sbom but dont follow symlinks
-syft dir:/tmp/dockerimage/uncompressed/ --base-path /tmp/dockerimage/uncompressed/ -o cyclonedx-json="/tmp/scans/sbom.json"
-
-
-
+umoci unpack --rootless --image /tmp/oci-image:latest /tmp/flattened-bundle
+syft dir:/tmp/flattened-bundle/rootfs/ --base-path /tmp/flattened-bundle/rootfs/ -o cyclonedx-json="/tmp/scans/sbom.json"
 grype /tmp/scans/sbom.json --sort-by=severity --output=template --template=/app/assets/tsv.tmpl --file=/tmp/scans/vulnerabilities.csv
+echo "Scanning Image $DOCKER_IMAGE Completed"
